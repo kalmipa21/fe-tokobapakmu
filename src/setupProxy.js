ea6f3d1adb4d8283a7625056496bf4cdc2602bc0
kalmipa21 <aripsapu@gmail.com>
2023-08-22 09:21:04 +0700

    delete const baseURL on setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const baseURL = {
-  target: process.env.REACT_APP_BASE_URL || "",
-  changeOrigin: true,
-  pathRewrite: {
-    "^/api": "",
-  },
-};
-
 const proxyWilayah = {
   target: process.env.REACT_APP_API_WILAYAH || "",
   changeOrigin: true,
@@ -17,6 +9,5 @@ const proxyWilayah = {
 };
 
 module.exports = function (app) {
-  app.use("/api", createProxyMiddleware(baseURL));
   app.use("/api-wilayah", createProxyMiddleware(proxyWilayah));
 };
